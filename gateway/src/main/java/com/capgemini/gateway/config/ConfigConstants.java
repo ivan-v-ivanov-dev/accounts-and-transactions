935b7fffc3fd924595e1aa5bfc4b243d6465613d
Ivan Ivanov <ivan.v.ivanovdev@gmail.com>
2024-07-24 11:05:21 +0300

    Gateway service: Add database

---
+++ gateway/src/main/java/com/capgemini/gateway/config/ConfigConstants.java
@@ -0,0 +1,13 @@
+package com.capgemini.gateway.config;
+
+public class ConfigConstants {
+
+    public static final String POSTGRES_CONNECTION_POOL = "Postgres Connection Pool";
+    public static final String POSTGRES_CONFIGURATION = "Postgres Configuration :: ";
+    public static final String POSTGRES_CONFIGURATION_HIKARI_DATA_SOURCE_CREATED = POSTGRES_CONFIGURATION + "Hikari DataSource Created";
+    public static final String POSTGRES_CONFIGURATION_LOCAL_CONTAINER_ENTITY_MANAGER_FACTORY = POSTGRES_CONFIGURATION + "LocalContainerEntityManagerFactory Created";
+    public static final String POSTGRES_CONFIGURATION_JPA_TRANSACTION_MANAGER_CREATED = POSTGRES_CONFIGURATION + "JpaTransactionManager Created";
+
+    private ConfigConstants() {
+    }
+}
